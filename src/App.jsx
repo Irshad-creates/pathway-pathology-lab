a0161f4ba6fd9eae34b996e530d804f51373c14a
Irshad-creates <ansariirshad78601@gmail.com>
2026-01-16 01:11:04 +0530

    staff ui almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StaffLogin from "./pages/auth/StaffLogin";
-import StaffLayout from "./layouts/StaffLayout";
-import Dashboard from "./pages/staff/Dashboard";
 import Reception from "./pages/staff/Reception";
-import Appointments from "./pages/staff/Appointments";
-import Billing from "./pages/staff/Billing";
+
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-
-        {/* Auth */}
-        <Route path="/login" element={<StaffLogin />} />
-
-        {/* Full-width Test Registration (NO sidebar) */}
-        <Route path="/staff/reception" element={<Reception />} />
-
-        {/* Staff layout pages */}
-        <Route path="/staff" element={<StaffLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="appointments" element={<Appointments />} />
-          <Route path="billing" element={<Billing />} />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Reception />;
 }
 
 export default App;
